d006c91658debdf356d0d8f02121f96faf293deb
bright_spark <markpassey@btinternet.com>
2016-08-02 16:59:09 +0100

    Updated to MC 1.10.2 and Forge 1.10.2-12.18.1.2011

--- src/main/java/com/brightspark/sparkshammers/reference/Reference.java
+++ src/main/java/com/brightspark/sparkshammers/reference/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
     public static final String MOD_ID = "SparksHammers";
     public static final String MOD_NAME = "Spark's Hammers";
-    public static final String VERSION = "1.9.4-1.3.1";
+    public static final String VERSION = "1.10.2-1.0";
     public static final String DEPENDENCIES =
             "after:EnderIO;" +
             "after:MobHunter;" +
