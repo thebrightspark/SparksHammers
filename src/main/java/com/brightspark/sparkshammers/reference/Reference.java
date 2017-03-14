a02d8d78d45c5755c923cad06afe84e45eb0ceec
bright_spark <markpassey@btinternet.com>
2017-03-14 01:43:46 +0000

    Updated version to 1.10.2-1.6.3

--- src/main/java/com/brightspark/sparkshammers/reference/Reference.java
+++ src/main/java/com/brightspark/sparkshammers/reference/Reference.java
@@ -9,7 +9,7 @@ public class Reference
 {
     public static final String MOD_ID = "SparksHammers";
     public static final String MOD_NAME = "Spark's Hammers";
-    public static final String VERSION = "1.10.2-1.6";
+    public static final String VERSION = "1.10.2-1.6.3";
     public static final String DEPENDENCIES =
             "after:EnderIO;" +
             "after:Botania;" +
